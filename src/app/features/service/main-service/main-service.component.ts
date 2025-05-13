cec96e02dd3ad4ae9bcfc668ef943783566fd10e
AnamShaikh <anam.adv.dev@gmail.com>
2025-05-14 01:56:48 +0200

    Homeservicesupdates

--- src/app/features/service/main-service/main-service.component.ts
+++ src/app/features/service/main-service/main-service.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CarbonreductionComponent } from '../../faq/carbonreduction/carbonreduction.component';
+import { CarbonreductionplanComponent } from '../carbonreductionplan/carbonreductionplan.component';
 
 @Component({
   selector: 'app-main-service',
-  imports: [],
+  imports: [CarbonreductionplanComponent],
   templateUrl: './main-service.component.html',
   styleUrl: './main-service.component.css'
 })
