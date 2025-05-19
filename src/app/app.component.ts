af2b1e41efc44dc5b4e3845ade757ac29be5d917
AnamShaikh <anam.adv.dev@gmail.com>
2025-05-19 18:10:11 +0200

    newchangesupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from './layout/footer/footer.component';
 import { PoliciesComponent } from "./features/policies/policies.component";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PoliciesComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PoliciesComponent,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'task1';
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        // ✅ Scroll to top on every route change
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+  }
+
 }
